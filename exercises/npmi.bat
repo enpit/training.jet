dd1b17955d56fd71983b0f54403c5c439f3e5600
Christian.Kunzmann@enpit.de <Christian.Kunzmann@enpit.de>
2017-05-11 19:39:48 +0200

    adding windows version of npm i script for convenience

---
+++ exercises/npmi.bat
@@ -0,0 +1,13 @@
+cd 1_hello-world
+call npm i
+cd ../2_modularisierung
+call npm i
+cd ../3_charts
+call npm i
+cd ../4_forms
+call npm i
+cd ..
+@echo off
+echo 'npm install' executed on all exercise workspaces.
+echo choose directory and type 'npm start' to start application.
+echo browser should open application automatically on 'http://localhost:8000/'
